6c153c403b033eea2f720058ad20a9311a6a87fa
tahakzed <tahakzed@gmail.com>
2021-01-30 23:15:53 +0500

    list of tasks-Done

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -7,17 +7,26 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView taskList;
+    Map<String,String> tasks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         taskList=findViewById(R.id.task_list);
-        TodoRecyclerViewAdapter adapter=new TodoRecyclerViewAdapter(new String[]{"Task 1 Task 1 Task 1 Task 1 Task 1 Task 1\n" +
-                "Task 1 Task 1 Task 1","Task 3  3 Task 3 Task 3" +
-                "Task 3 Task 3 Task 3 " ,"Task 4 Task 4 Task 4 Task 4 Task 4 Task 4 "});
+        tasks=new HashMap<>();
+        tasks.put("Task 1[important]","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum");
+        tasks.put("Task 2[important]","It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.");
+        tasks.put("Task 3[important]","Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
+        tasks.put("Task 4[sw_important]","not that important .. i just have to get some groceries done thats it");
+        tasks.put("Task 5[filler]","I will do it if I find time otherwise it doesn't matter .. its kinda a filler");
+        TodoRecyclerViewAdapter adapter=new TodoRecyclerViewAdapter(tasks);
         taskList.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
         taskList.setAdapter(adapter);
     }
--- app/src/main/java/com/example/todoapp/TodoRecyclerViewAdapter.java
+++ app/src/main/java/com/example/todoapp/TodoRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.todoapp;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,21 +10,33 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Map;
+
 public class TodoRecyclerViewAdapter extends RecyclerView.Adapter<TodoRecyclerViewAdapter.ViewHolder> {
-    private String[] taskList;
+    private Map<String,String> tasks;
+
     public static class ViewHolder extends RecyclerView.ViewHolder{
         private final TextView taskTv;
+        private final TextView taskTitle;
+        private final CardView cardView;
+        private final View underline;
         public ViewHolder(View view){
             super(view);
             taskTv=view.findViewById(R.id.task_tv);
+            taskTitle=view.findViewById(R.id.task_title);
+            cardView=view.findViewById(R.id.cardViewTask);
+            underline=view.findViewById(R.id.underline);
         }
         public TextView getTaskTv(){
             return taskTv;
         }
+        public TextView getTaskTitle(){return  taskTitle;}
+        public CardView getCardView(){return cardView;}
+        public View getUnderline(){return underline;}
     }
 
-    public TodoRecyclerViewAdapter(String[] taskList){
-        this.taskList=taskList;
+    public TodoRecyclerViewAdapter(Map<String,String> tasks){
+        this.tasks=tasks;
     }
     @NonNull
     @Override
@@ -35,11 +48,42 @@ public class TodoRecyclerViewAdapter extends RecyclerView.Adapter<TodoRecyclerVi
 
     @Override
     public void onBindViewHolder(@NonNull TodoRecyclerViewAdapter.ViewHolder holder, int position) {
-        holder.getTaskTv().setText(taskList[position]);
+        String temp=tasks.keySet().toArray()[position].toString();
+        String taskType=temp.substring(temp.indexOf("[")+1,temp.length()-1);
+        String key=temp.substring(0,temp.indexOf("["));
+        TextView title=holder.getTaskTitle();
+        TextView task=holder.getTaskTv();
+        CardView card= holder.getCardView();
+        View ul=holder.getUnderline();
+        if(taskType.equals("important"))
+        {
+            card.setCardBackgroundColor(card.getContext().getResources().getColor(R.color.important));
+            title.setTextColor(title.getContext().getResources().getColor(R.color.important_text));
+            ul.setBackgroundColor(ul.getContext().getResources().getColor(R.color.important_text));
+            task.setTextColor(task.getContext().getResources().getColor(R.color.important_text));
+        }
+        else if(taskType.equals("sw_important"))
+        {
+            card.setCardBackgroundColor(card.getContext().getResources().getColor(R.color.sw_important));
+            title.setTextColor(title.getContext().getResources().getColor(R.color.sw_important_text));
+            ul.setBackgroundColor(ul.getContext().getResources().getColor(R.color.sw_important_text));
+            task.setTextColor(task.getContext().getResources().getColor(R.color.sw_important_text));
+        }
+        else if(taskType.equals("filler"))
+        {
+            card.setCardBackgroundColor(card.getContext().getResources().getColor(R.color.filler));
+            title.setTextColor(title.getContext().getResources().getColor(R.color.filler_text));
+            ul.setBackgroundColor(ul.getContext().getResources().getColor(R.color.filler_text));
+            task.setTextColor(task.getContext().getResources().getColor(R.color.filler_text));
+        }
+
+        title.setText(key);
+        task.setText(tasks.get(temp));
+
     }
 
     @Override
     public int getItemCount() {
-        return taskList.length;
+        return tasks.size();
     }
 }
